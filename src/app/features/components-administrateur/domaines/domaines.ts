795b5e2ff514f02ce8d21e01c172120b0abc35fd
Deku5005 <dolooumar60@gmail.com>
2025-08-04 11:05:57 +0000

    Creation de la page domaine et projet

--- src/app/features/components-administrateur/domaines/domaines.ts
+++ src/app/features/components-administrateur/domaines/domaines.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPen,faTrash } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-domaines',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule],
   templateUrl: './domaines.html',
   styleUrl: './domaines.css'
 })
 export class Domaines {
-
+  faPen = faPen; 
+  faTrash = faTrash; 
 }
