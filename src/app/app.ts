7ac35d1ed8a89ea7551c55fd9f83f82eaad7ec14
Mamoutou02 <sangaremoutou.360@.com>
2025-08-06 15:39:08 +0000

    Mes projet chez contributeurs

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,14 @@ import {RouterLink, RouterOutlet} from '@angular/router';
 import {DasboardAdmin} from './features/components-administrateur/dasboard-admin/dasboard-admin';
 import {SidebarContributeurs} from './features/coponents-contributeurs/sidebar-contributeurs/sidebar-contributeurs';
 import {Sidebar} from './features/components-administrateur/sidebar/sidebar';
+import {
+  MesProjetContributeurs
+} from './features/coponents-contributeurs/mes-projet-contributeurs/mes-projet-contributeurs';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DasboardAdmin, RouterLink, Sidebar, SidebarContributeurs],
+  imports: [RouterOutlet, DasboardAdmin, RouterLink, Sidebar, SidebarContributeurs, MesProjetContributeurs],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
